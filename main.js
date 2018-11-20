3fe8f649103abff054cfbf960176519fbd870520
Adrian Jungnelius <a.jungnelius@gmail.com>
2018-11-20 13:54:06 +0100

    js
    added shuffle function on refresh of page

--- main.js
+++ main.js
@@ -1,42 +1,50 @@
 
 'use strict=1';
 
+
 const cards = [
-  { id: 1, image: './images/apple.png' },
-  { id: 1, image: './images/banana.png' },
-  { id: 2, image: './images/carrot.png' },
-  { id: 2, image: './images/strawberry.png' },
-  { id: 3, image: './images/orange.png' },
-  { id: 3, image: './images/grapes.png' },
-  { id: 4, image: './images/grapes.png' },
-  { id: 4, image: './images/grapes.png' },
-  { id: 5, image: './images/grapes.png' },
-  { id: 5, image: './images/grapes.png' },
-  { id: 6, image: './images/grapes.png' },
-  { id: 6, image: './images/grapes.png' },
-  { id: 7, image: './images/grapes.png' },
-  { id: 7, image: './images/grapes.png' },
-  { id: 8, image: './images/grapes.png' },
-  { id: 8, image: './images/kiwi.png' }
+  { id: 1, cardclass: 'cards card-1', image: './images/apple.png' },
+  { id: 1, cardclass: 'cards card-1', image: './images/apple.png' },
+  { id: 2, cardclass: 'cards card-2', image: './images/carrot.png' },
+  { id: 2, cardclass: 'cards card-2', image: './images/carrot.png' },
+  { id: 3, cardclass: 'cards card-3', image: './images/orange.png' },
+  { id: 3, cardclass: 'cards card-3', image: './images/orange.png' },
+  { id: 4, cardclass: 'cards card-4', image: './images/grapes.png' },
+  { id: 4, cardclass: 'cards card-5', image: './images/grapes.png' },
+  { id: 5, cardclass: 'cards card-5', image: './images/strawberry.png' },
+  { id: 5, cardclass: 'cards card-6', image: './images/strawberry.png' },
+  { id: 6, cardclass: 'cards card-6', image: './images/.png' },
+  { id: 6, cardclass: 'cards card-7', image: './images/grapes.png' },
+  { id: 7, cardclass: 'cards card-7', image: './images/grapes.png' },
+  { id: 7, cardclass: 'cards card-8', image: './images/grapes.png' },
+  { id: 8, cardclass: 'cards card-8', image: './images/kiwi.png' },
+  { id: 8, cardclass: 'cards card-4', image: './images/kiwi.png' }
 ]
-const container = document.querySelector('.container')
-const cardsArr = [];
 let firstcard = 0;
 let secondcard = 0;
-
+const container = document.querySelector('.container')
+const cardsArr = [];
 const shufflebutton = document.querySelector('.shuffle')
-
 for (var i = cards.length; i--; cardsArr.unshift(cards[i]));
 
-const makeCard = (id,image) => {
-	return `<div class="cards" data-id="${id}">
+const makeCard = (id,cardclass, image) => {
+	return `<div data-id="${id}" class="${cardclass}">
 	</div>`;
 }
 
-for (let i = 0; i < cards.length; i++) {
-	const card = makeCard(cards[i].id, cards[i].image);
-	container.innerHTML += card;
-};
+const removecards = () => {
+	container.innerHTML = "";
+}
+function printcards(cardsArr){
+    for (let i = 0; i < cardsArr.length; i++) {
+        const card = makeCard(cardsArr[i].id, cardsArr[i].cardclass, cardsArr[i].image);
+        container.innerHTML += card;
+    };
+}
+
+shuffle(cardsArr);
+removecards(cardsArr);
+printcards(cardsArr);
 
 function shuffle(array) {
 	for (let i = array.length - 1; i > 0; i--) {
@@ -45,32 +53,27 @@ function shuffle(array) {
 	}
 }
 
-shufflebutton.addEventListener('click', () => {
-	shuffle(cardsArr);
-	console.log(cardsArr);
+document.querySelectorAll('.cards').forEach((card) => {
 
-})
-//
-// const checkMatch = () => {
-// 	return firstcard === secondcard;
-// }
-//
-//
-// Array.from(cards).forEach((card) => {
-//   card.addEventListener('click', () => {
-// 		console.log("click")
-// 		card.classList.toggle('active');
-// 		if (firstcard === 0) {
-// 			firstcard = card.dataset.cardId
-// 		}
-// 		else {
-// 			secondcard = card.dataset.cardId
-// 			if (checkMatch(firstcard, secondcard)) {
-//
-// 				console.log("same");
-// 			}
-// 			firstcard = 0;
-// 			secondcard = 0;
-// 		}
-// 	})
-// });
+	card.addEventListener('click', () => {
+
+		console.log("click")
+		card.classList.toggle('active');
+
+		if (firstcard === 0) {
+			firstcard = card.dataset.dataId
+		}
+		else {
+			secondcard = card.dataset.dataId
+			if (checkMatch(firstcard, secondcard)) {
+				console.log("same");
+			}
+			firstcard = 0;
+			secondcard = 0;
+		}
+	})
+});
+
+const checkMatch = () => {
+	return firstcard === secondcard;
+}
