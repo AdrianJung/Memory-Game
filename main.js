3038d9e147f5167fdd2a92d95886c6d85a2462f6
Adrian Jungnelius <a.jungnelius@gmail.com>
2018-11-20 18:19:54 +0100

    fixed js
    cleaned up unecessary javascript

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 
 'use strict=1';
 
-
 const cards = [
   { id: 1, cardclass: 'cards card-1', image: './images/apple.png' },
   { id: 2, cardclass: 'cards card-1', image: './images/apple.png' },
@@ -10,38 +9,32 @@ const cards = [
   { id: 7, cardclass: 'cards card-3', image: './images/orange.png' },
   { id: 8, cardclass: 'cards card-3', image: './images/orange.png' },
   { id: 10, cardclass: 'cards card-4', image: './images/grapes.png' },
-  { id: 11, cardclass: 'cards card-5', image: './images/grapes.png' },
+  { id: 11, cardclass: 'cards card-4', image: './images/grapes.png' },
   { id: 13, cardclass: 'cards card-5', image: './images/strawberry.png' },
-  { id: 14, cardclass: 'cards card-6', image: './images/strawberry.png' },
+  { id: 14, cardclass: 'cards card-5', image: './images/strawberry.png' },
   { id: 16, cardclass: 'cards card-6', image: './images/.png' },
-  { id: 17, cardclass: 'cards card-7', image: './images/grapes.png' },
+  { id: 17, cardclass: 'cards card-6', image: './images/grapes.png' },
   { id: 19, cardclass: 'cards card-7', image: './images/grapes.png' },
-  { id: 20, cardclass: 'cards card-8', image: './images/grapes.png' },
+  { id: 20, cardclass: 'cards card-7', image: './images/grapes.png' },
   { id: 22, cardclass: 'cards card-8', image: './images/kiwi.png' },
-  { id: 23, cardclass: 'cards card-4', image: './images/kiwi.png' }
+  { id: 23, cardclass: 'cards card-8', image: './images/kiwi.png' }
 ]
+
+shuffle(cards);
 let firstcard = 0;
 let secondcard = 0;
 const container = document.querySelector('.container')
-const cardsArr = [];
 const shufflebutton = document.querySelector('.shuffle')
-for (var i = cards.length; i--; cardsArr.unshift(cards[i]));
-const removecards = () => {
-	container.innerHTML = "";
-}
+const done = [];
 const makeCard = (id,cardclass, image) => {
 	return `<div data-id="${id}" class="${cardclass}">
 	</div>`;
 }
-function printcards(cardsArr){
-    for (let i = 0; i < cardsArr.length; i++) {
-        const card = makeCard(cardsArr[i].id, cardsArr[i].cardclass, cardsArr[i].image);
-        container.innerHTML += card;
-    };
-}
-shuffle(cardsArr);
-removecards(cardsArr);
-printcards(cardsArr);
+
+for (let i = 0; i < cards.length; i++) {
+    const card = makeCard(cards[i].id, cards[i].cardclass, cards[i].image);
+    container.innerHTML += card;
+};
 
 function shuffle(array) {
 	for (let i = array.length - 1; i > 0; i--) {
@@ -50,26 +43,35 @@ function shuffle(array) {
 	}
 }
 
-const checkMatch = (firstcard, secondcard) => {
-	if (firstcard-1 == secondcard || secondcard-1 == firstcard) {
-		console.log("HEJ")
-	}
-}
+let isselected = 0
 
 document.querySelectorAll('.cards').forEach((card) => {
+	const checkMatch = (firstcard, secondcard) => {
+		if (firstcard-1 == secondcard || secondcard-1 == firstcard) {
+			card.classList.add('active')
+		} else {
+			console.log("no match")
+		}
+	}
 	card.addEventListener('click', () => {
-		card.classList.toggle('active');
-		if (firstcard === 0) {
-			firstcard = card.dataset.id
-			console.log(firstcard)
-		}
-		else {
-			secondcard = card.dataset.id
-			console.log(secondcard)
-			checkMatch(firstcard, secondcard)
+		console.log(isselected)
+		if (isselected == 2) {
+			console.log("You already chose 2!")
 
-			firstcard = 0;
-			secondcard = 0;
+		} else {
+			isselected++
 		}
+
+			if (firstcard === 0) {
+				firstcard = card.dataset.id
+
+			}
+			else {
+				secondcard = card.dataset.id
+				checkMatch(firstcard, secondcard)
+				isselected = 0;
+				firstcard = 0;
+				secondcard = 0;
+			}
 	})
 });
