d2022bb2859568e3ebb036e88d07e504e3d3a8dc
Adrian Jungnelius <a.jungnelius@gmail.com>
2018-11-27 10:42:44 +0100

    fixed resetbutton and some javascript issues with card selecting

--- main.js
+++ main.js
@@ -1,40 +1,38 @@
-
 'use strict=1';
 
 const cards = [
-  { id: 1, cardclass: 'cards card-1', image: './images/apple.png' },
-  { id: 2, cardclass: 'cards card-1', image: './images/apple.png' },
-  { id: 4, cardclass: 'cards card-2', image: './images/carrot.png' },
-  { id: 5, cardclass: 'cards card-2', image: './images/carrot.png' },
-  { id: 7, cardclass: 'cards card-3', image: './images/orange.png' },
-  { id: 8, cardclass: 'cards card-3', image: './images/orange.png' },
-  { id: 10, cardclass: 'cards card-4', image: './images/grapes.png' },
-  { id: 11, cardclass: 'cards card-4', image: './images/grapes.png' },
-  { id: 13, cardclass: 'cards card-5', image: './images/strawberry.png' },
-  { id: 14, cardclass: 'cards card-5', image: './images/strawberry.png' },
-  { id: 16, cardclass: 'cards card-6', image: './images/.png' },
-  { id: 17, cardclass: 'cards card-6', image: './images/grapes.png' },
-  { id: 19, cardclass: 'cards card-7', image: './images/grapes.png' },
-  { id: 20, cardclass: 'cards card-7', image: './images/grapes.png' },
-  { id: 22, cardclass: 'cards card-8', image: './images/kiwi.png' },
-  { id: 23, cardclass: 'cards card-8', image: './images/kiwi.png' }
+  { id: 1, cardclass: 'card card-1', image: './images/apple.png' },
+  { id: 1, cardclass: 'card card-1', image: './images/apple.png' },
+  { id: 2, cardclass: 'card card-2', image: './images/carrot.png' },
+  { id: 2, cardclass: 'card card-2', image: './images/carrot.png' },
+  { id: 3, cardclass: 'card card-3', image: './images/orange.png' },
+  { id: 3, cardclass: 'card card-3', image: './images/orange.png' },
+  { id: 4, cardclass: 'card card-4', image: './images/grapes.png' },
+  { id: 4, cardclass: 'card card-4', image: './images/grapes.png' },
+  { id: 5, cardclass: 'card card-5', image: './images/strawberry.png' },
+  { id: 5, cardclass: 'card card-5', image: './images/strawberry.png' },
+  { id: 6, cardclass: 'card card-6', image: './images/.png' },
+  { id: 6, cardclass: 'card card-6', image: './images/grapes.png' },
+  { id: 7, cardclass: 'card card-7', image: './images/grapes.png' },
+  { id: 7, cardclass: 'card card-7', image: './images/grapes.png' },
+  { id: 8, cardclass: 'card card-8', image: './images/kiwi.png' },
+  { id: 8, cardclass: 'card card-8', image: './images/kiwi.png' }
 ]
 
-shuffle(cards);
 let firstcard = 0;
+let previoustarget = 0;
 let secondcard = 0;
+let counter = 0;
 const container = document.querySelector('.container')
-const shufflebutton = document.querySelector('.shuffle')
-const done = [];
-const makeCard = (id,cardclass, image) => {
+const shufflebutton = document.querySelector('.reset')
+const makeCard = (id, cardclass, image) => {
 	return `<div data-id="${id}" class="${cardclass}">
 	</div>`;
 }
 
-for (let i = 0; i < cards.length; i++) {
-    const card = makeCard(cards[i].id, cards[i].cardclass, cards[i].image);
-    container.innerHTML += card;
-};
+const removecards = () => {
+	container.innerHTML = "";
+}
 
 function shuffle(array) {
 	for (let i = array.length - 1; i > 0; i--) {
@@ -43,35 +41,61 @@ function shuffle(array) {
 	}
 }
 
-let isselected = 0
+function printcards(cards) {
+	for (let i = 0; i < cards.length; i++) {
+		const card = makeCard(cards[i].id, cards[i].cardclass, cards[i].image);
+		container.innerHTML += card;
+	};
+}
 
-document.querySelectorAll('.cards').forEach((card) => {
-	const checkMatch = (firstcard, secondcard) => {
-		if (firstcard-1 == secondcard || secondcard-1 == firstcard) {
-			card.classList.add('active')
-		} else {
-			console.log("no match")
+shuffle(cards);
+removecards(cards);
+printcards(cards);
+
+shufflebutton.addEventListener('click', () => {
+	removecards(cards);
+	shuffle(cards);
+	printcards(cards);
+})
+
+const checkMatch = (firstcard, secondcard, firstcardEle, secondcardEle) => {
+	if (firstcard === secondcard) {
+		finishCounter++
+		if (finishCounter == 8) {
+
 		}
+
+	} else {
+		firstcardEle.classList.remove('active')
+		secondcardEle.classList.remove('active')
 	}
-	card.addEventListener('click', () => {
-		console.log(isselected)
-		if (isselected == 2) {
-			console.log("You already chose 2!")
+}
+let finishCounter = 0;
+const clickFix = (e) => {
+	if (e.target.matches('.card')) {
 
-		} else {
-			isselected++
-		}
+		if (previoustarget !== e.target || counter == 0) {
+			e.target.classList.add('active')
+				++counter
 
 			if (firstcard === 0) {
-				firstcard = card.dataset.id
+				firstcard = e.target.dataset.id
+				firstcardEle = e.target
+			} else {
+				secondcard = e.target.dataset.id
+				secondcardEle = e.target
+				if (counter == 2) {
+					setTimeout(() => {
+						checkMatch(firstcard, secondcard, firstcardEle, secondcardEle)
+						counter = 0;
+						firstcard = 0;
+						secondcard = 0;
+					}, 600)
+				}
+			}
+		}
+		previoustarget = e.target
+	}
+}
 
-			}
-			else {
-				secondcard = card.dataset.id
-				checkMatch(firstcard, secondcard)
-				isselected = 0;
-				firstcard = 0;
-				secondcard = 0;
-			}
-	})
-});
+container.addEventListener('click', clickFix)
